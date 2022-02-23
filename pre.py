5d3236e3f35aa7ed2535693ba997ef2a59581f65
pren1 <33309499+pren1@users.noreply.github.com>
2022-02-23 14:48:15 -0800

    Update pre.py

--- pre.py
+++ pre.py
@@ -73,4 +73,4 @@ if __name__ == "__main__":
     del args.n_processes_embed
     preprocess_dataset(**vars(args))
     
-    create_embeddings(synthesizer_root=args.out_dir, n_processes=n_processes_embed, encoder_model_fpath=encoder_model_fpath)    
+    # create_embeddings(synthesizer_root=args.out_dir, n_processes=n_processes_embed, encoder_model_fpath=encoder_model_fpath)    
